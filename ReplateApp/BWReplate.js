c2aaa67cd1d5b3be8640cb9a71e5177e1953e7c1
kodebae <karmenuopx@outlook.com>
2020-07-26 15:54:04 -0400

    Boilerplate for the Frontend of the App

---
+++ ReplateApp/BWReplate.js
@@ -0,0 +1,1 @@
+/** This is the main app compoent (top level component) */ 
---
+++ ReplateApp/notesForPeteAndKarmen.js
@@ -0,0 +1,14 @@
+/** Pete ~ Here is a place for us to write notes where we can share information about our code since I am not sure when you will be available due to your busy schedule this week, I figured this would be a good way to leave a breadcrumb trail for you. :)
+ * ! Karmen notes - Sunday 8/22/20:
+ * ? I have created Biolerplate code for the Frontend of the App. Main app component can be found at --> 'BWReplate.js'. <-- Business and Volunteer folders can be found inside of the 'Components' folder when ready to proceed with project. Buildpack will still need to be added when app is deployed. I also created the file for the Volunteer Profile page inside of the Volunteer folder to help get you started.
+ * ? I am thinking of styling our main componets with 'Styled Components'. I am open to any changes being suggested and discussed as early as Tuesday night. I would like to have all styling finialized before then. My goal is to have the FE of the app completly finished by Wednesy night and ready to deploy and debugg by Thursday Night. Does that sound ok to you?
+ * ? I have done research and have chosen to use 'React Hook Forms' for my form components. This should work easily with Redux and React. I do not forsee any issues with it since my forms will be rendered seperatly from the volunteer components. However, I am not an expert and would like feedback on all of this. Also, how do you feel about using functional componets? I would prefer to use functional components wherever we can. 
+ * TODO: I installed a few dependencies: 
+ * * 1. Create React App, 
+ * * 2. React Router DOM, 
+ * * 3. React Hook Form, 
+ * * 4. YUP for validation,
+ * * 5. React redux,
+ * * 6. Axios
+ 
+ */
